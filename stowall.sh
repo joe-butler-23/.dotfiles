99bb6b7b1766e21a71be6940f0e64374e09991e6
Joe Butler <130206285+joe-butler-23@users.noreply.github.com>
2025-10-08 15:19:27 +0100

    Update stowall.sh

--- stowall.sh
+++ stowall.sh
@@ -5,6 +5,10 @@ DOTFILES_DIR="$HOME/.dotfiles"
 # Ensure the script always runs from the correct directory
 cd "$DOTFILES_DIR" || { echo "❌ Failed to change directory to $DOTFILES_DIR"; exit 1; }
 
+# Ensure target directories exist
+mkdir -p "$HOME"
+mkdir -p "$HOME/.config"
+
 echo "🔍 Checking and stowing individual dotfiles..."
 stow -v -t "$HOME" home
 
