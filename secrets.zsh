2b02a5adafeed70742c97dc3a535ac8656e6994f
joe-butler-23 <joeb.92@hotmail.co.uk>
2025-10-29 16:51:33 +0000

    Integrate Home Manager and migrate config to xdg.configFile layout

--- secrets.zsh
+++
@@ -1,6 +0,0 @@
-# op inject references
-
-export TODOIST_API_KEY="op://APIs/Todoist API Key/password"
-export OPENAI_API_KEY="op://APIs/OpenAI API Key/password"
-export ANTHROPIC="op://APIs/Anthropic API Key/password"
-export GEMINI_API_KEY="op://APIs/Gemini API Key/password"
